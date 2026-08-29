7353f22615bf2cb7089d23a12080dea2713a92c7
Ashu Prakash <aspksh@gmail.com>
2026-08-29 12:08:53 +0530

    Update faq_app.py

--- faq_app.py
+++ faq_app.py
@@ -6,7 +6,7 @@ import requests
 # FastAPI URL
 # ---------------------------------------
 
-FASTAPI_URL = "https://bbs-world-expansys-section.trycloudflare.com/question"
+FASTAPI_URL = "https://pda-nights-jump-reached.trycloudflare.com/question"
 
 
 # ---------------------------------------
